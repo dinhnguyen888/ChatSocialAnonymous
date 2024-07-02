223443c0f13b3ba3e46b5827db836f533dfe4bfc
DinhNguyen <nguyenphucdinh89@gmail.com>
2024-07-03 01:26:34 +0700

    modified:   .env 	modified:   Database/db.ts

--- Database/db.ts
+++ Database/db.ts
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-const uri:string = 'mongodb://localhost:27017/database'; 
+const uri: string = process.env.MONGODB_URI as string;
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log('Connected to MongoDB');
+        console.log('Connected to MongoDB Atlas');
     })
-    .catch((err:Error) => {
-        console.error('Error connecting to MongoDB', err);
+    .catch((err: Error) => {
+        console.error('Error connecting to MongoDB Atlas', err);
     });
 
 export default mongoose;
